77244e191101515da6f42813c7f970c5c7fd77f3
Rohan Kamath <rohan.kamath9@gmail.com>
2019-03-16 15:16:03 +0000

    Code written to initalise a network

--- networks.py
+++ networks.py
@@ -1,10 +1,25 @@
 import numpy as np
 import networkx as nx
-
+import matplotlib.pyplot as plt
 class network():
-    def __init__(self,):
-        self.vertices = []
+    def __init__(self,m):
+        self.vertices = range(m+1)
+        self.edges = []
+        for i in self.vertices:
+            edges_i = []
+            for j in self.vertices:
+                if i != j:
+                    edges_i.append(j)
+            self.edges.append(edges_i)
 
+    def plot(self):
+        graph = nx.Graph()
+        graph.add_nodes_from(self.vertices)
+        for i in range(len(self.edges)):
+            for j in self.edges[i]:
+                graph.add_edge(i,j)
+        plt.plot()
+        nx.draw(graph)
+        plt.show()
+        return
 
-#test
-git
